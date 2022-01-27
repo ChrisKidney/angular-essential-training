b9d2ac867b2f1a25c2e525bf4df69d3cc3e35708
ChrisKidney <christopherkidney@hotmail.com>
2022-01-27 17:02:47 -0400

    4. Forms - Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
+import { MediaItemFormComponent } from './media-item-form.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        ReactiveFormsModule
     ],
     declarations: [
         AppComponent,
         MediaItemComponent,
         MediaItemListComponent,
+        MediaItemFormComponent,
         FavoriteDirective,
         CategoryListPipe
     ],
